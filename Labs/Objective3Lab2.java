13dd3114af053162a7fa62edb374e976f331f185
Henrymelendez <henryomarm@gmail.com>
2022-02-23 16:19:13 -0500

    completed objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    side1 = 10;
+    side2 = 8;
+    double squared1 = side1 * side1;
+    double squared2 = side2 * side2;
+    hypotenuse = Math.sqrt(squared1 + squared2);
+    System.out.println(hypotenuse);
+  }
+}
