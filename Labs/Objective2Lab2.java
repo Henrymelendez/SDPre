0dda74f38a3f1f83bc0d7187b995dd3a328556c4
Henrymelendez <henryomarm@gmail.com>
2022-02-23 15:37:43 -0500

    completed objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Henry Melendez";
+    int age = 32;
+    String state = "Massachusetts";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
