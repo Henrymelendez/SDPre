761e929a1da08952c1abc98d1a27762f8e08ddc1
Henrymelendez <henryomarm@gmail.com>
2022-02-23 22:46:26 -0500

    completed objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,16 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    //TODO Write a for loop that will print the numbers 1-10
+    for(int i = 0; i <= 10; i++){
+      System.out.println(i);
+    }
+
+    for(int j = 10; j >= 0; j--){
+      System.out.println(j);
+    }
+
+    //TODO Write another for loop that will print the numbers 10-1.
+
+
+  }
+}
