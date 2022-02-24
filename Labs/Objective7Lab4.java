23751cf34af598ae5c1e33d32de02c9644cef1cd
Henrymelendez <henryomarm@gmail.com>
2022-02-23 21:36:44 -0500

    completed objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int sum, count;
+
+    count = 0;
+    sum = 0;
+
+
+    while(count <= 20){
+      sum += count;
+      count ++;
+    }
+    System.out.println(sum);
+  }
+}
